77aa8eb77a0881ac05ff52f2367554934df8db96
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-11-11 22:11:39 +0100

    Removing AA

--- BeerCounter/src/org/androidannotations/devoxx/beer/BeerCounterActivity.java
+++ BeerCounter/src/org/androidannotations/devoxx/beer/BeerCounterActivity.java
@@ -2,38 +2,62 @@ package org.androidannotations.devoxx.beer;
 
 import static android.text.Html.fromHtml;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.text.SpannableStringBuilder;
 import android.text.style.ImageSpan;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
-import com.googlecode.androidannotations.annotations.AfterViews;
-import com.googlecode.androidannotations.annotations.Background;
-import com.googlecode.androidannotations.annotations.Click;
-import com.googlecode.androidannotations.annotations.EActivity;
-import com.googlecode.androidannotations.annotations.OptionsItem;
-import com.googlecode.androidannotations.annotations.OptionsMenu;
-import com.googlecode.androidannotations.annotations.UiThread;
-import com.googlecode.androidannotations.annotations.ViewById;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 
-@EActivity(R.layout.beers)
-@OptionsMenu(R.menu.beer_menu)
 public class BeerCounterActivity extends SherlockActivity {
 
 	int beerCount;
 
-	@ViewById
 	TextView beerCountView;
 
-	@AfterViews
-	@Background
-	void loadBeerCount() {
-		SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-		int beerCount = preferences.getInt("beerCount", 0);
-		beerCountLoaded(beerCount);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.beers);
+
+		beerCountView = (TextView) findViewById(R.id.beerCountView);
+
+		View addBeerButton = findViewById(R.id.addBeerButton);
+		addBeerButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				addBeerButtonClicked();
+			}
+		});
+
+		loadBeerCount();
 	}
 
-	@UiThread
+	void loadBeerCount() {
+		new AsyncTask<Void, Void, Integer>() {
+
+			@Override
+			protected Integer doInBackground(Void... params) {
+				SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+				int beerCount = preferences.getInt("beerCount", 0);
+				return beerCount;
+			}
+
+			@Override
+			protected void onPostExecute(Integer beerCount) {
+				beerCountLoaded(beerCount);
+			}
+		}.execute();
+	}
+
 	void beerCountLoaded(int beerCount) {
 		this.beerCount = beerCount;
 		updateBeerViews();
@@ -60,22 +84,45 @@ public class BeerCounterActivity extends SherlockActivity {
 		}
 	}
 
-	@Click
 	void addBeerButtonClicked() {
 		beerCount++;
 		saveBeerCount(beerCount);
 		updateBeerViews();
 	}
 
-	@Background
-	void saveBeerCount(int beerCount) {
-		getPreferences(MODE_PRIVATE) //
-				.edit() //
-				.putInt("beerCount", beerCount) //
-				.commit();
+	void saveBeerCount(final int beerCount) {
+
+		new AsyncTask<Void, Void, Void>() {
+
+			@Override
+			protected Void doInBackground(Void... params) {
+				getPreferences(MODE_PRIVATE) //
+						.edit() //
+						.putInt("beerCount", beerCount) //
+						.commit();
+				return null;
+			}
+
+		}.execute();
+
 	}
 
-	@OptionsItem
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getSupportMenuInflater();
+		menuInflater.inflate(R.menu.beer_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.emergency) {
+			emergencySelected();
+			return true;
+		}
+		return false;
+	}
+
 	void emergencySelected() {
 		beerCount = 0;
 		saveBeerCount(beerCount);
